166a7e5bb2b6d6c96fd195e9cf04147561f93701
Numaan Ali <alinumaan35@gmail.com>
2025-10-21 23:08:30 -0700

    ko

--- routers/servicesrouter.js
+++ routers/servicesrouter.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const express = require ("express");
 const { getservicesmodels, createserviesmodels, updateserviesmodels, deleteserviesmodels } = require("../controllers/Servicescontroller");
 
 
 const router = express.Router();
 
-router.get("/servicesmodels", getservicesmodels);
-router.post("/servicesmodels", createserviesmodels);
-router.put("/servicesmodels/:id", updateserviesmodels);
-router.delete("/servicesmodels/:id", deleteserviesmodels);
 
-module.exports = router;
+router.get("/servicemodels", getservicesmodels)
+router.post("/servicemodels", createserviesmodels)
+router.put("/servicemodels/:id", updateserviesmodels)
+router.delete("/servicemodels/:id", deleteserviesmodels)
+
+
+module.exports = router
